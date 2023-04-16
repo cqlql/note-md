ba436be74d43247fde48777b4599919567696572
cqlql <cql.ql@qq.com>
2023-04-16 06:34:34 +0000

    Deploying to gh-pages from @ cqlql/note-md@9ded708a020e4fdeb297c63237b3672ebed87eb0 🚀

---
+++ assets/git 操作.html.e92b8f06.js
@@ -0,0 +1,29 @@
+import{_ as n}from"./_plugin-vue_export-helper.cdc0426e.js";import{o as s,a,f as t}from"./app.9a73a5f3.js";const e={},p=t(`<p>\u4F7F\u7528 GitPython \u5E93</p><p>\u53C2\u8003\u6587\u6863</p><p>https://gitpython.readthedocs.io/en/stable/tutorial.html</p><p>https://www.cnblogs.com/wanglan/p/10718876.html</p><p>https://www.jianshu.com/p/c1a7a32ae50b</p><div class="language-python ext-py line-numbers-mode"><pre class="language-python"><code><span class="token keyword">import</span> git
+
+
+<span class="token keyword">def</span> <span class="token function">commit</span><span class="token punctuation">(</span><span class="token punctuation">)</span><span class="token punctuation">:</span>
+    repo <span class="token operator">=</span> git<span class="token punctuation">.</span>Repo<span class="token punctuation">(</span><span class="token string">&#39;./.git&#39;</span><span class="token punctuation">)</span>
+
+    <span class="token keyword">if</span> repo<span class="token punctuation">.</span>is_dirty<span class="token punctuation">(</span><span class="token punctuation">)</span><span class="token punctuation">:</span>
+        repo<span class="token punctuation">.</span>remote<span class="token punctuation">(</span><span class="token punctuation">)</span><span class="token punctuation">.</span>fetch<span class="token punctuation">(</span><span class="token punctuation">)</span>
+        <span class="token comment"># try:</span>
+        <span class="token comment">#   repo.remote().pull()</span>
+        <span class="token comment"># except:</span>
+        <span class="token comment">#     print(&#39;pull\u9519\u8BEF&#39;)</span>
+
+        repo<span class="token punctuation">.</span>git<span class="token punctuation">.</span>add<span class="token punctuation">(</span><span class="token string">&#39;-A&#39;</span><span class="token punctuation">)</span>
+
+        <span class="token comment"># repo.index.add([&#39;*&#39;])</span>
+        <span class="token comment"># repo.index.remove([&#39;*&#39;])</span>
+
+        <span class="token comment"># repo.index.commit(f&quot;update {datetime.datetime.now()}&quot;)</span>
+        repo<span class="token punctuation">.</span>index<span class="token punctuation">.</span>commit<span class="token punctuation">(</span><span class="token string">&#39;.&#39;</span><span class="token punctuation">)</span>
+
+        repo<span class="token punctuation">.</span>remote<span class="token punctuation">(</span><span class="token punctuation">)</span><span class="token punctuation">.</span>push<span class="token punctuation">(</span><span class="token punctuation">)</span>
+
+        <span class="token keyword">print</span><span class="token punctuation">(</span><span class="token string">&#39;push \u5B8C\u6210&#39;</span><span class="token punctuation">)</span>
+
+
+commit<span class="token punctuation">(</span><span class="token punctuation">)</span>
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,6),i=[p];function c(o,l){return s(),a("div",null,i)}const r=n(e,[["render",c],["__file","git \u64CD\u4F5C.html.vue"]]);export{r as default};
---
+++ assets/用 REST API 测试 HTTP 请求.html.e26ac182.js
@@ -0,0 +1,14 @@
+import{_ as i}from"./_plugin-vue_export-helper.cdc0426e.js";import{o,a as s,b as e,e as a,c as t,f as r,r as l}from"./app.9a73a5f3.js";const d={},c=r(`<h2 id="\u4F7F\u7528\u53D8\u91CF\u5B9E\u73B0-token-\u8BF7\u6C42\u7684\u4F8B\u5B50" tabindex="-1"><a class="header-anchor" href="#\u4F7F\u7528\u53D8\u91CF\u5B9E\u73B0-token-\u8BF7\u6C42\u7684\u4F8B\u5B50" aria-hidden="true">#</a> \u4F7F\u7528\u53D8\u91CF\u5B9E\u73B0 token \u8BF7\u6C42\u7684\u4F8B\u5B50</h2><p>example.http \u6587\u4EF6</p><div class="language-text ext-text line-numbers-mode"><pre class="language-text"><code># @name login
+POST https://uc.youzhuanla.com/login HTTP/2
+content-type: application/json
+{
+  &quot;username&quot;:&quot;chenqiaoli&quot;,
+  &quot;password&quot;:&quot;lmm789&quot;,
+  &quot;company_name&quot;:&quot;1&quot;
+}
+
+###
+@authToken = {{login.response.body.data.token}}
+https://uc.youzhuanla.com/online/get?pagenum=10&amp;page=1
+authorization: Bearer {{authToken}}
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="\u53C2\u8003\u6587\u6863" tabindex="-1"><a class="header-anchor" href="#\u53C2\u8003\u6587\u6863" aria-hidden="true">#</a> \u53C2\u8003\u6587\u6863</h2>`,4),u={href:"https://marketplace.visualstudio.com/items?itemName=humao.rest-client",target:"_blank",rel:"noopener noreferrer"},m={href:"https://zhuanlan.zhihu.com/p/382740857",target:"_blank",rel:"noopener noreferrer"};function h(v,p){const n=l("ExternalLinkIcon");return o(),s("div",null,[c,e("p",null,[e("a",u,[a("REST \u5BA2\u6237\u7AEF - Visual Studio Marketplace"),t(n)])]),e("p",null,[e("a",m,[a("VSCode\u7684REST Client\u6307\u5357\uFF0C\u8D85\u597D\u7528\u7684HTTP\u5BA2\u6237\u7AEF\u5DE5\u5177 - \u77E5\u4E4E (zhihu.com)"),t(n)])])])}const T=i(d,[["render",h],["__file","\u7528 REST API \u6D4B\u8BD5 HTTP \u8BF7\u6C42.html.vue"]]);export{T as default};
